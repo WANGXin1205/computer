bd4cd0ce42c60a20e1e4e077d2ef3fa2603d10e0
wangxin <394315185@qq.com>
2018-07-02 21:26:10 +0800

    讨论了多线程的资源共享 volatile sleep join等方法。

--- src/main/java/com/growlithe/computer/mysql/practice/customer/thread/TicketSynchronizedRunnable.java
+++ src/main/java/com/growlithe/computer/mysql/practice/customer/thread/TicketSynchronizedRunnable.java
@@ -3,6 +3,7 @@ package com.growlithe.computer.mysql.practice.customer.thread;
 import org.springframework.scheduling.concurrent.ThreadPoolExecutorFactoryBean;
 
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.locks.Lock;
 
 /**
  * @Author : Growlithe
@@ -34,9 +35,15 @@ public class TicketSynchronizedRunnable implements Runnable {
      * 也可以这么写 public synchronized void run(){}
      */
     @Override
-    public void run() {
-        synchronized (this){
-            // tickets 一定要有this才可以加锁
+    public  void run() {
+        synchronized (this.tickets){
+            /*
+            tickets 一定要有this才可以加锁 然后线程1执行完毕，才会释放锁，开始执行线程2
+            实际上，synchronized(this)以及非static的synchronized方法（至于static synchronized方法请往下看），
+            只能防止多个线程同时执行同一个对象的同步代码段。
+            synchronized锁住的是代码还是对象。答案是：synchronized锁住的是括号里的对象，而不是代码。
+            对于非static的synchronized方法，锁的就是对象本身也就是this。
+            */
             while (this.tickets > 0) {
                 System.out.println("线程" + threadName + "销售出一张票，剩余： " + --this.tickets);
             }
@@ -53,5 +60,6 @@ public class TicketSynchronizedRunnable implements Runnable {
         ThreadFactory threadFactory = new ThreadPoolExecutorFactoryBean();
         threadFactory.newThread(ticketSynchronizedRunnable).start();
         threadFactory.newThread(ticketSynchronizedRunnable1).start();
+
     }
 }
